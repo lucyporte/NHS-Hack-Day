b1cb1ed66651d74ae33c5223e71c5e90c8b0f8cd
lucyporte <lucymtporte@gmail.com>
2025-03-24 22:24:19 +0000

    Add all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FamilyDiseaseTracker from './components/FamilyDiseaseTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <FamilyDiseaseTracker />
+    </div>
+  );
+}
+
+export default App;
